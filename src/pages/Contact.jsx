072086bda4b953602d1994aa927e1a91749b7ad3
rii98 <136151503+rii98@users.noreply.github.com>
2025-02-24 11:20:39 +0545

    deploy

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
         <img className='w-full md:max-w-[450px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
-          <p className='font-semibold text-xl text-gray-600'>Our Store kere kha</p>
+          <p className='font-semibold text-xl text-gray-600'>Our Store </p>
           <p className='text-gray-500'> Boudha <br/>Kathmandu, Nepal </p>
           <p className='text-gray-500'>Tel: 986000000 <br />Email: fashionnepal@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Fashion Nepal</p>
